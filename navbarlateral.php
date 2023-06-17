b2e591b3a8bf7f78a9005c2a2f7fdba58d64e816
Matheus Mariano <matheus_pm97@yahoo.com.br>
2023-06-16 22:50:32 -0300

    sidebar completa

--- navbarlateral.php
+++ navbarlateral.php
@@ -1,40 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-	<title>Clínica Médica</title>
-<body>
-            <div class="col-md-3 bg-dark vh-100">
-            <nav class="navbar navbar-expand-md navbar-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sideMenu"
-        aria-controls="sideMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="sideMenu">
-        <ul class="navbar-nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link" href="inicio.php">Inicio</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="cadastrar_funcionario.php">Novo Funcionário</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="listar_medico.php">Médicos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="marcar_consulta.php">Marcar Consulta</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="cadastro_procedimento.php">Procedimentos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="buscar_consultas.php">Buscar Consultas</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="buscar_exames.php">Resultado de Exames</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="style.css">
+        <style>
+        .sidebar-expanded .list-group-item {
+            border: none;
+        }
 
-            </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-</body>
+        .sidebar-collapsed .list-group-item {
+            border: none;
+        }
+        </style>
+    </head>
+
+    <body>
+        <div id="sidebar-container" class="sidebar-expanded d-none d-md-block">
+            <ul class="list-group">
+                <a href="#submenu1" data-toggle="collapse" aria-expanded="false"
+                    class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
+                    <span class="fa fa-dashboard fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Dashboard</span>
+                    <span class="submenu-icon ml-auto"></span>
+                </a>
+                <a href="listar_medico.php"
+                    class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
+                    <span><i class="bi bi-heart-pulse fs-5"></i></span>
+                    <span class="fa fa-user fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Médicos</span>
+                </a>
+                <a href="listar_paciente.php"
+                    class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
+                    <span><i class="bi bi-people-fill fs-5"></i></span>
+                    <span class="fa fa-user fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Pacientes</span>
+                </a>
+                <a href="cadastrar_funcionario.php"
+                    class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
+                    <span><i class="bi bi-person-vcard-fill fs-5"></i></span>
+                    <span class="fa fa-user fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Funcionários</span>
+                </a>
+                <a href="marcar_consulta.php" class="bg-dark list-group-item list-group-item-action">
+                    <span><i class="bi bi-calendar-month fs-5"></i></span>
+                    <span class="fa fa-tasks fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Marcar Consulta</span>
+                </a>
+                <a href="cadastro_procedimento.php" class="bg-dark list-group-item list-group-item-action">
+                    <span><i class="bi bi-bandaid fs-5"></i></span>
+                    <span class="fa fa-calendar fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Procedimentos</span>
+                </a>
+                <a href="buscar_consultas.php" class="bg-dark list-group-item list-group-item-action">
+                    <span><i class="bi bi-calendar-check fs-5"></i></span>
+                    <span class="fa fa-envelope-o fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Consultas <span
+                            class="badge badge-pill badge-primary ml-2">5</span></span>
+                </a>
+                <a href="buscar_exames.php" class="bg-dark list-group-item list-group-item-action">
+                    <span><i class="bi bi-ui-checks fs-5"></i></span>
+                    <span class="fa fa-question fa-fw mr-3"></span>
+                    <span class="menu-collapsed">Exames</span>
+                </a>
+                <a href="#top" data-toggle="sidebar-colapse"
+                    class="bg-dark list-group-item list-group-item-action d-flex align-items-center">
+                    <span><i class="bi bi-arrow-left-right fs-5"></i></span>
+                    <span id="collapse-icon" class="fa fa-2x mr-3"></span>
+                    <span id="collapse-text" class="menu-collapsed">Collapse</span>
+                </a>
+            </ul>
+        </div>
+
+        <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            // Hide submenus
+            document.querySelectorAll('#body-row .collapse').forEach(function(collapse) {
+                collapse.classList.remove('show');
+            });
+
+            // Collapse/Expand icon
+            var collapseIcon = document.getElementById('collapse-icon');
+            collapseIcon.classList.add('fa-angle-double-left');
+
+            // Collapse click
+            document.querySelectorAll('[data-toggle="sidebar-colapse"]').forEach(function(element) {
+                element.addEventListener('click', function() {
+                    SidebarCollapse();
+                });
+            });
+
+            function SidebarCollapse() {
+                var menuCollapsedItems = document.querySelectorAll('.menu-collapsed');
+                var sidebarSubmenus = document.querySelectorAll('.sidebar-submenu');
+                var submenuIcons = document.querySelectorAll('.submenu-icon');
+                var sidebarContainer = document.getElementById('sidebar-container');
+
+                menuCollapsedItems.forEach(function(element) {
+                    element.classList.toggle('d-none');
+                });
+                sidebarSubmenus.forEach(function(element) {
+                    element.classList.toggle('d-none');
+                });
+                submenuIcons.forEach(function(element) {
+                    element.classList.toggle('d-none');
+                });
+                sidebarContainer.classList.toggle('sidebar-expanded');
+                sidebarContainer.classList.toggle('sidebar-collapsed');
+
+                // Treating d-flex/d-none on separators with title
+                var separatorTitles = document.querySelectorAll('.sidebar-separator-title');
+                separatorTitles.forEach(function(separatorTitle) {
+                    separatorTitle.classList.toggle('d-flex');
+                });
+
+                // Collapse/Expand icon
+                if (sidebarContainer.classList.contains('sidebar-collapsed')) {
+                    collapseIcon.classList.remove('fa-angle-double-left');
+                    collapseIcon.classList.add('fa-angle-double-right');
+                } else {
+                    collapseIcon.classList.remove('fa-angle-double-right');
+                    collapseIcon.classList.add('fa-angle-double-left');
+                }
+            }
+        });
+        </script>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+        </script>
+    </body>
