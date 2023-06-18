75c7be6594f7140f12611790ca252733ae097c8a
Matheus Mariano <matheus_pm97@yahoo.com.br>
2023-06-17 21:39:25 -0300

    Ajuste do botao de status, navbar superior e tabela de dados

--- buscar_exames.php
+++ buscar_exames.php
@@ -18,7 +18,7 @@ $exames = filtrarExames($conn, $id, $status, $nomeExame, $nomePaciente, $nomeMed
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -31,81 +31,80 @@ $exames = filtrarExames($conn, $id, $status, $nomeExame, $nomePaciente, $nomeMed
         <div class="container-fluid">
             <div class="row">
                 <?php include('navbarlateral.php'); ?>
-                <div class="col-md-9">
-                    <div class="container mt-5">
-                        <h1>Exames</h1>
-                        <form method="POST" id="filtro-form" style="margin-bottom: 20px;">
-                            <div class="form-row">
-                                <div class="col">
-                                    <input type="text" name="nome_exame" class="form-control"
-                                        placeholder="Nome do Exame" value="<?php echo $nomeExame; ?>">
-                                </div>
-                                <div class="col">
-                                    <input type="text" name="nome_paciente" class="form-control"
-                                        placeholder="Nome do Paciente" value="<?php echo $nomePaciente; ?>">
-                                </div>
-                                <div class="col">
-                                    <input type="text" name="nome_medico" class="form-control"
-                                        placeholder="Nome do Médico" value="<?php echo $nomeMedico; ?>">
-                                </div>
-                                <div class="col">
 
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" role="switch" id="filter_btn">
-                                        <label class="form-check-label " for="filter_btn">
-                                            <span class="slider"></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="d-flex">
-                                        <button type="submit" name="filtrar" class="btn btn-primary">Filtrar</button>
-                                    </div>
+                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+                    <h1>Exames</h1>
+                    <form method="POST" id="filtro-form" style="margin-bottom: 20px;">
+                        <div class="form-row">
+                            <div class="col">
+                                <input type="text" name="nome_exame" class="form-control" placeholder="Nome do Exame"
+                                    value="<?php echo $nomeExame; ?>">
+                            </div>
+                            <div class="col">
+                                <input type="text" name="nome_paciente" class="form-control"
+                                    placeholder="Nome do Paciente" value="<?php echo $nomePaciente; ?>">
+                            </div>
+                            <div class="col">
+                                <input type="text" name="nome_medico" class="form-control" placeholder="Nome do Médico"
+                                    value="<?php echo $nomeMedico; ?>">
+                            </div>
+                            <div class="col">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" role="switch" id="filter_btn">
+                                    <label class="form-check-label" for="filter_btn">
+                                        <span class="slider text-center" id="slider_text">off</span>
+                                    </label>
                                 </div>
                             </div>
-                        </form>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Nome do Exame</th>
-                                    <th>Nome do Paciente</th>
-                                    <th>Nome do Médico</th>
-                                    <th>Status</th>
-                                    <th>Ação</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($exames as $exame): ?>
-                                <tr class="exame-row">
-                                    <td><?php echo $exame['nome_exame']; ?></td>
-                                    <td><?php echo $exame['paciente_nome']; ?></td>
-                                    <td><?php echo $exame['medico_nome']; ?></td>
-                                    <td class="status-cell">
-                                        <?php if ($exame['status'] == 1): ?>
-                                        Pendente
-                                        <?php elseif ($exame['status'] == 2): ?>
-                                        Concluído
-                                        <?php endif; ?>
-                                    </td>
-                                    <td>
-                                        <?php if ($exame['status'] == 1 && $_SESSION['status'] == 2): ?>
-                                        <form method="POST" style="display: inline-block;">
-                                            <input type="hidden" name="id_exame" value="<?php echo $exame['id']; ?>">
-                                            <button type="submit" name="alterar_status"
-                                                class="btn btn-outline-success">Disponibilizar</button>
-                                        </form>
-                                        <?php endif; ?>
-                                    </td>
-                                </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-                <script>
-                var filterBtn = document.getElementById("filter_btn");
-                filterBtn.addEventListener("change", toggleFilter);
-                </script>
+                            <div class="col">
+                                <div class="d-flex">
+                                    <button type="submit" name="filtrar" class="btn btn-primary">Filtrar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Nome do Exame</th>
+                                <th>Nome do Paciente</th>
+                                <th>Nome do Médico</th>
+                                <th>Status</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($exames as $exame): ?>
+                            <tr class="tela-user-row">
+                                <td><?php echo $exame['nome_exame']; ?></td>
+                                <td><?php echo $exame['paciente_nome']; ?></td>
+                                <td><?php echo $exame['medico_nome']; ?></td>
+                                <td class="status-cell">
+                                    <?php if ($exame['status'] == 1): ?>
+                                    Pendente
+                                    <?php elseif ($exame['status'] == 2): ?>
+                                    Concluído
+                                    <?php endif; ?>
+                                </td>
+                                <td>
+                                    <?php if ($exame['status'] == 1 && $_SESSION['status'] == 2): ?>
+                                    <form method="POST" style="display: inline-block;">
+                                        <input type="hidden" name="id_exame" value="<?php echo $exame['id']; ?>">
+                                        <button type="submit" name="alterar_status"
+                                            class="btn btn-outline-success">Disponibilizar</button>
+                                    </form>
+                                    <?php endif; ?>
+                                </td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </main>
+            </div>
+        </div>
+        
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="./script.js"></script>
     </body>
+
 </html>
--- funcoes_exame.php
+++ funcoes_exame.php
@@ -87,28 +87,3 @@ Function filtrarExames($conn, $id, $status, $nomeExame, $nomePaciente, $nomeMedi
 }
 
 ?>
-<script>
-function toggleFilter() {
-    var filterBtn = document.getElementById("filter_btn");
-    var isFilterActive = filterBtn.checked;
-
-    if (isFilterActive) {
-        // Código para aplicar o filtro e mostrar somente os exames concluídos
-        var rows = document.getElementsByClassName("exame-row");
-        for (var i = 0; i < rows.length; i++) {
-            var statusCell = rows[i].querySelector(".status-cell");
-            if (statusCell.textContent.trim() !== "Concluído") {
-                rows[i].style.display = "none";
-            }
-        }
-    } else {
-        // Código para remover o filtro e mostrar todos os exames
-        var rows = document.getElementsByClassName("exame-row");
-        for (var i = 0; i < rows.length; i++) {
-            rows[i].style.display = "";
-        }
-    }
-}
-
-
-</script>
